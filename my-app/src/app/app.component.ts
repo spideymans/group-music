3b2fe16b5cfd7eb715c2d4e3cce76e739ff63770
Louis Mark <louis.mark@ontariotechu.net>
2020-12-08 02:10:45 -0500

    Improveed app.component.ts formatting

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -1,213 +1,252 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { AngMusicPlayerComponent } from 'ang-music-player';
-import {webSocket, WebSocketSubject} from 'rxjs/webSocket';
+import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
 import { WebSocketService } from './web-socket.service';
 import { interval } from 'rxjs';
-import { filter, map } from 'rxjs/operators'
+import { filter, map } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'my-app';
 
-  @ViewChild(AngMusicPlayerComponent) musicPlayerComponent: AngMusicPlayerComponent;
+  @ViewChild(AngMusicPlayerComponent)
+  musicPlayerComponent: AngMusicPlayerComponent;
   filterEvents: boolean = false;
   fileToUpload: File = null;
-  public userName = ""
-  audioList: QueueItem[] = [ ];
+  public userName = '';
+  audioList: QueueItem[] = [];
 
-  constructor(private webSocketService: WebSocketService, private httpClient: HttpClient, private toastr: ToastrService) { 
+  constructor(
+    private webSocketService: WebSocketService,
+    private httpClient: HttpClient,
+    private toastr: ToastrService
+  ) {}
 
+  ngOnInit() {
+    this.setupSocketConnection();
   }
 
-  ngOnInit() { 
-    console.log("Initializing connection");
-    this.webSocketService.listen('test event').subscribe((data) => { 
-      console.log(`Connected with id: ${this.webSocketService.socket.id}`)
+  setupSocketConnection() {
+    console.log('Initializing connection');
+    this.webSocketService.listen('test event').subscribe((data) => {
+      console.log(`Connected with id: ${this.webSocketService.socket.id}`);
       this.toastr.success('Connection Successful', null, {
         closeButton: true,
-        positionClass:'bottom-left'
+        positionClass: 'bottom-left',
       });
     });
 
-    console.log(this.audioList.length >= 1)
+    console.log(this.audioList.length >= 1);
 
-    // ---- Incoming events ---- 
+    // ---- Incoming events ----
 
-  this.webSocketService.listen("playEvent").subscribe((data) => {
-    console.log(`playEvent recieved. Sender: ${data.senderID}`);
-    this.timeout();
-    this.musicPlayerComponent.play();
-    this.toast(`${data.userName} played`)
+    this.webSocketService.listen('playEvent').subscribe((data) => {
+      console.log(`playEvent recieved. Sender: ${data.senderID}`);
+      this.timeout();
+      this.musicPlayerComponent.play();
+      this.toast(`${data.userName} played`);
     });
 
-  this.webSocketService.listen("pauseEvent").subscribe((data) => {
-    console.log(`pauseEvent recieved. Sender: ${data.senderID}`);
-    this.timeout();
-    this.musicPlayerComponent.pause();
-    this.toast(`${data.userName} paused`)
-  });
+    this.webSocketService.listen('pauseEvent').subscribe((data) => {
+      console.log(`pauseEvent recieved. Sender: ${data.senderID}`);
+      this.timeout();
+      this.musicPlayerComponent.pause();
+      this.toast(`${data.userName} paused`);
+    });
 
-  this.webSocketService.listen("nextEvent").subscribe((data) => {
-    console.log(`nextEvent recieved. Sender: ${data.senderID}`);
-    this.timeout();
-    this.musicPlayerComponent.nextAudio();
-    this.toast(`${data.userName} skipped`)
-  });
+    this.webSocketService.listen('nextEvent').subscribe((data) => {
+      console.log(`nextEvent recieved. Sender: ${data.senderID}`);
+      this.timeout();
+      this.musicPlayerComponent.nextAudio();
+      this.toast(`${data.userName} skipped`);
+    });
 
-  this.webSocketService.listen("previousEvent").subscribe((data) => {
-    console.log(`previousEvent recieved. Sender: ${data.senderID}`);
-    this.timeout();
-    this.musicPlayerComponent.previousAudio();
-    this.toast(`${data.userName} played previous song`)
-  });
+    this.webSocketService.listen('previousEvent').subscribe((data) => {
+      console.log(`previousEvent recieved. Sender: ${data.senderID}`);
+      this.timeout();
+      this.musicPlayerComponent.previousAudio();
+      this.toast(`${data.userName} played previous song`);
+    });
 
-  this.webSocketService.listen("seekEvent").subscribe((data) => {
-    console.log(`seekEvent recieved. Sender: ${data.senderID}`);
-    // AngMusicPlayer requires the new time to be located in seekAudioValue.target.value. Do not modify the structure of this object.
-    const seekAudioValue = { 
-      target: { 
-        value: data.newTime
-      }
-    }
+    this.webSocketService.listen('seekEvent').subscribe((data) => {
+      console.log(`seekEvent recieved. Sender: ${data.senderID}`);
+      // AngMusicPlayer requires the new time to be located in seekAudioValue.target.value. Do not modify the structure of this object.
+      const seekAudioValue = {
+        target: {
+          value: data.newTime,
+        },
+      };
 
-    this.timeout();
-    this.musicPlayerComponent.seekAudio(seekAudioValue);
-    this.toast(`${data.userName} seeked`)
-  });
+      this.timeout();
+      this.musicPlayerComponent.seekAudio(seekAudioValue);
+      this.toast(`${data.userName} seeked`);
+    });
 
-  this.webSocketService.listen("deleteEvent").subscribe((data) => { 
-    console.log(`Delete event recieved for song with ID ${data.songID}. Sender: ${data.senderID}`)
-    this.audioList = this.audioList.filter((element) => element.id != data.songID)
-    this.toast(`${data.userName} deleted a song.`)
-  })
+    this.webSocketService.listen('deleteEvent').subscribe((data) => {
+      console.log(
+        `Delete event recieved for song with ID ${data.songID}. Sender: ${data.senderID}`
+      );
+      this.audioList = this.audioList.filter(
+        (element) => element.id != data.songID
+      );
+      this.toast(`${data.userName} deleted a song.`);
+    });
 
-  this.webSocketService.listen("addEvent").subscribe((data) => { 
-    console.log(`Add event recieved for song with ID ${data.songMetadata.id}. Sender: ${data.senderID}`);
-    this.audioList.push(data.songMetadata);
-    this.toast(`${data.userName} added song.`)
-  })
+    this.webSocketService.listen('addEvent').subscribe((data) => {
+      console.log(
+        `Add event recieved for song with ID ${data.songMetadata.id}. Sender: ${data.senderID}`
+      );
+      this.audioList.push(data.songMetadata);
+      this.toast(`${data.userName} added song.`);
+    });
 
-  this.webSocketService.listen("sample message").subscribe((data) => {
-    console.log("sample message recieved!");
-    // this.musicPlayerComponent.pause();
-  });
-
+    this.webSocketService.listen('sample message').subscribe((data) => {
+      console.log('sample message recieved!');
+      // this.musicPlayerComponent.pause();
+    });
   }
 
-  
-    // ---- AngMusicPlayer events ---- 
-    // These events are triggered by AngMusicPlayer when the corresponding actions are taken
+  // ---- AngMusicPlayer events ----
+  // These events are triggered by AngMusicPlayer when the corresponding actions are taken
 
   playEvent() {
-    console.log(this.userName)
-    if (!this.filterEvents) { 
-      console.log("Emitting playEvent")
-      this.webSocketService.emit("playEvent", { senderID: this.webSocketService.socket.id, userName: this.userName });
+    console.log(this.userName);
+    if (!this.filterEvents) {
+      console.log('Emitting playEvent');
+      this.webSocketService.emit('playEvent', {
+        senderID: this.webSocketService.socket.id,
+        userName: this.userName,
+      });
     }
   }
 
   pauseEvent() {
     if (!this.filterEvents) {
-      console.log("Emitting pauseEvent")
-      this.webSocketService.emit("pauseEvent", { senderID: this.webSocketService.socket.id, userName: this.userName });
+      console.log('Emitting pauseEvent');
+      this.webSocketService.emit('pauseEvent', {
+        senderID: this.webSocketService.socket.id,
+        userName: this.userName,
+      });
     }
   }
 
-  nextEvent() { 
-    if (!this.filterEvents) { 
-      console.log("Emitting nextEvent")
-      this.webSocketService.emit("nextEvent", { senderID: this.webSocketService.socket.id, userName: this.userName });
+  nextEvent() {
+    if (!this.filterEvents) {
+      console.log('Emitting nextEvent');
+      this.webSocketService.emit('nextEvent', {
+        senderID: this.webSocketService.socket.id,
+        userName: this.userName,
+      });
     }
   }
 
   previousEvent() {
-    if (!this.filterEvents) { 
-      console.log("Emitting previousEvent")
-      this.webSocketService.emit("previousEvent", { senderID: this.webSocketService.socket.id, userName: this.userName });
+    if (!this.filterEvents) {
+      console.log('Emitting previousEvent');
+      this.webSocketService.emit('previousEvent', {
+        senderID: this.webSocketService.socket.id,
+        userName: this.userName,
+      });
     }
   }
 
   seekEvent() {
-    const newTime = this.musicPlayerComponent.audioPlayer.nativeElement.currentTime;
-    if (!this.filterEvents) { 
-      console.log("Emitting seekEvent");
-      this.webSocketService.emit("seekEvent", { senderID: this.webSocketService.socket.id, userName: this.userName, newTime: newTime });
+    const newTime = this.musicPlayerComponent.audioPlayer.nativeElement
+      .currentTime;
+    if (!this.filterEvents) {
+      console.log('Emitting seekEvent');
+      this.webSocketService.emit('seekEvent', {
+        senderID: this.webSocketService.socket.id,
+        userName: this.userName,
+        newTime: newTime,
+      });
     }
   }
 
-  sendMsg() { 
-    this.webSocketService.emit("sample message", { senderID: this.webSocketService.socket.id });
+  sendMsg() {
+    this.webSocketService.emit('sample message', {
+      senderID: this.webSocketService.socket.id,
+    });
   }
-  
+
   /**
    * Call timeout() before calling play(), pause(), nextAudio() or previousAudio() on AngMusicPlayer.
    * timeout() will ignore all events emitted by AndMusicPlayer for 25 ms.
    */
-  timeout() { 
+  timeout() {
     this.filterEvents = true;
-    setTimeout(()=>{
+    setTimeout(() => {
       this.filterEvents = false;
-    }, 250)
+    }, 250);
   }
 
   addToQueue(title: String, url: String) {
     const metadata: QueueItem = {
-      id: Math.floor((Math.random() * 1000000) + 1),
+      id: Math.floor(Math.random() * 1000000 + 1),
       url: url,
       title: title,
-      cover: "https://i1.sndcdn.com/artworks-000249294066-uow7s0-t500x500.jpg"
+      cover: 'https://i1.sndcdn.com/artworks-000249294066-uow7s0-t500x500.jpg',
     };
     this.audioList.push(metadata);
-    this.webSocketService.emit("addEvent", {  senderID: this.webSocketService.socket.id,
-                                              userName: this.userName,
-                                              songMetadata: metadata
-                                            });
+    this.webSocketService.emit('addEvent', {
+      senderID: this.webSocketService.socket.id,
+      userName: this.userName,
+      songMetadata: metadata,
+    });
   }
-  
-  delete(songID: number) { 
-    this.audioList = this.audioList.filter((element) => element.id != songID)
-    this.webSocketService.emit("deleteEvent", { senderID: this.webSocketService.socket.id,
-                                                songID: songID,
-                                                userName: this.userName });
+
+  delete(songID: number) {
+    this.audioList = this.audioList.filter((element) => element.id != songID);
+    this.webSocketService.emit('deleteEvent', {
+      senderID: this.webSocketService.socket.id,
+      songID: songID,
+      userName: this.userName,
+    });
   }
 
   handleFileInput(files: FileList) {
-    const file = files.item(0)
-    if (file == null) { return }
+    const file = files.item(0);
+    if (file == null) {
+      return;
+    }
 
     const formData = new FormData();
     formData.append('song', file);
-   
-    this.httpClient.post(`http://localhost:${this.webSocketService.port}/`, formData)
-      .subscribe(res => {
+
+    this.httpClient
+      .post(`http://localhost:${this.webSocketService.port}/`, formData)
+      .subscribe((res) => {
         console.log(res);
         alert('Added song to queue successfully.');
 
-        this.addToQueue(file.name ,`http://localhost:${this.webSocketService.port}/${file.name}`)
-      })
+        this.addToQueue(
+          file.name,
+          `http://localhost:${this.webSocketService.port}/${file.name}`
+        );
+      });
   }
 
-  toast(text: string) { 
+  toast(text: string) {
     this.toastr.success(text, null, {
       closeButton: true,
-      positionClass:'bottom-left'
+      positionClass: 'bottom-left',
     });
   }
 }
 
-export interface Message { 
-  message: String
+export interface Message {
+  message: String;
 }
 
 export interface QueueItem {
-  id: number,
-  url: String,
-  title: String,
-  cover: String
+  id: number;
+  url: String;
+  title: String;
+  cover: String;
 }
